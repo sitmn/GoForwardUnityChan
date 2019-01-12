0010f484031600459ef2b30da719b7065b189649
sitmn <sho-t-jpx375@docomo.ne.jp>
2019-01-12 17:02:46 +0900

    Lesson7 課題
    障害物と床が衝突する際効果音が鳴るよう、AudioSourceからPlayOneShotを使用するスクリプトを、CubeControllerに追加

---
+++ Assets/Script/CubeController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+    private float speed = -0.2f;
+
+    private float deadLine = -10;
+
+    private AudioSource audiosource;
+
+    public AudioClip block;
+
+	// Use this for initialization
+	void Start () {
+        
+        this.audiosource = GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(this.speed, 0 ,0);
+
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "CubeTag" || other.gameObject.tag == "GroundTag")
+        {
+            this.audiosource.PlayOneShot(block);
+            
+        }
+    }
+}
